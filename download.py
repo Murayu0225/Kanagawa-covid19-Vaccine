a5c97441072b578a0981da7aa6be009bc474908e
Yu Muramatsu <murayu@assistant-app.com>
2021-07-21 15:13:04 +0900

    Update download.py

--- download.py
+++ download.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver import Chrome, ChromeOptions, Remote
-# 待機に必要
 import time
 
 options = webdriver.ChromeOptions()
@@ -8,12 +7,9 @@ options.add_argument('--headless')
 options.add_experimental_option("prefs", {
   "download.default_directory": "Data/"
 })
-# prefs オプションを設定、デフォルトのダウンロードディレクトリ変更
 driver = webdriver.Chrome(options=options)
-time.sleep(1) # 秒 なくても動いた念のため
+time.sleep(1)
 driver.get("https://www.pref.kanagawa.jp/docs/ga4/covid19/vaccine.html")
-# CSVファイルのサンプルダウンロードサイト
 driver.find_element_by_partial_link_text(u"こちら（CSV：3KB）（別ウィンドウで開きます）").click()
-time.sleep(5) # 秒
+time.sleep(5)
 driver.quit()
-# ブラウザーを終了する。
